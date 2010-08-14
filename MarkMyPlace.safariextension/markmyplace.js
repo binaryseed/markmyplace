8e1cf549221ac54141a1bc1d78f2d115dcae3b2a
Vince Foley <vincefoley@gmail.com>
2010-08-13 19:02:55 -0500

    follows page resize

--- MarkMyPlace.safariextension/markmyplace.js
+++ MarkMyPlace.safariextension/markmyplace.js
@@ -4,13 +4,42 @@ if(window.top==window)
 	marker.setAttribute('id','mmp_marker')
 	document.body.insertBefore(marker,document.body.firstChild)
 	
+	var element
+	
+	var clickLocation, elementLocation
+	
 	document.addEventListener("click",function(event){
 		
 		if(event.button==1 && event.target.nodeName!="A")
 		{
+			element = event.target
+			clickLocation = event.pageY
+			
+			marker.style.height = "20px"
+			marker.style.visibility = "visible"
 			marker.style.top = (event.pageY-10)+"px"
-			if (event.target.id=="mmp_marker") { marker.style.top = "-100px" }
+			if (event.target.id=="mmp_marker") { marker.style.visibility = "hidden" }
 		}
 		
+	}, false)
+	
+	window.addEventListener("resize",function(event){
+		
+		if (!element) { return }
+		
+		var curtop = 0
+		var obj = element
+		
+		while (obj.offsetTop==obj.parentNode.offsetTop) { obj = obj.parentNode }
+		
+		do { curtop += obj.offsetTop } while ( obj = obj.offsetParent )
+		elementLocation = curtop
+		
+		if (Math.abs(elementLocation-clickLocation)<200)
+		{
+			marker.style.top = (elementLocation+"px")
+			if(element.offsetHeight>20) { marker.style.height = element.offsetHeight+"px" }
+		}
+
 	}, false)
 }
